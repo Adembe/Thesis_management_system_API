4d6f0852e7f672226f125e45746aa9024ccc383b
Dembereldodov <adembereldodov1210@gmail.com>
2024-04-28 23:43:12 +0800

    fixed

--- server/routes/router.go
+++ server/routes/router.go
@@ -23,40 +23,40 @@ func ConfigRoutes(router *gin.Engine) *gin.Engine {
 		}
 		thesis := main.Group("/thesis")
 		{
-			thesis.GET("/:id", controllers.GetThesis)
-			thesis.POST("/", controllers.CreateThesis)
-			thesis.GET("/own/:teacher_id", controllers.GetOwnThesis)
-			thesis.DELETE("/:id", controllers.DeleteThesis)
-			thesis.PUT("/", controllers.UpdateThesis)
-			thesis.GET("/allrequested/:teacher_id", controllers.GetAllRequested)
+			thesis.GET(":id", controllers.GetThesis)
+			thesis.POST("", controllers.CreateThesis)
+			thesis.GET("own/:teacher_id", controllers.GetOwnThesis)
+			thesis.DELETE(":id", controllers.DeleteThesis)
+			thesis.PUT("", controllers.UpdateThesis)
+			thesis.GET("allrequested/:teacher_id", controllers.GetAllRequested)
 		}
 		thesisOffice := main.Group("/thesis-office")
 		{
-			thesisOffice.GET("/", controllers.GetAllthesis)
-			thesisOffice.PUT("/", controllers.UpdateReqThesis)
+			thesisOffice.GET("", controllers.GetAllthesis)
+			thesisOffice.PUT("", controllers.UpdateReqThesis)
 		}
 		student := main.Group("/student")
 		{
-			student.GET("/", controllers.GetStudentThesis)
-			student.PUT("/", controllers.StudentReqThesis)
-			student.GET("/:student_id", controllers.GetStudentShowReq)
+			student.GET("", controllers.GetStudentThesis)
+			student.PUT("", controllers.StudentReqThesis)
+			student.GET(":student_id", controllers.GetStudentShowReq)
 		}
 		process := main.Group("/process")
 		{
-			process.POST("/", controllers.InsertProcess)
-			process.GET("/student/:student_id", controllers.GetProcessStudent)
-			process.POST("/detail", controllers.InsertProcessDetail)
-			process.GET("/detail/:process_id", controllers.GetProcessDetail)
-			process.PUT("/student", controllers.UpdateFeedbackStudent)
+			process.POST("", controllers.InsertProcess)
+			process.GET("student/:student_id", controllers.GetProcessStudent)
+			process.POST("detail", controllers.InsertProcessDetail)
+			process.GET("detail/:process_id", controllers.GetProcessDetail)
+			process.PUT("student", controllers.UpdateFeedbackStudent)
 		}
 
 		processTandS := main.Group("/processTandS")
 		{
-			processTandS.GET("/", controllers.GetProcessAll)
-			processTandS.GET("/teacher/:teacher_id", controllers.GetProcessTeacher)
-			processTandS.PUT("/", controllers.UpdateProcessOne)
-			processTandS.PUT("/all", controllers.UpdateProcessAll)
-			processTandS.PUT("/teacher", controllers.UpdateFeedbackTeacher)
+			processTandS.GET("", controllers.GetProcessAll)
+			processTandS.GET("teacher/:teacher_id", controllers.GetProcessTeacher)
+			processTandS.PUT("", controllers.UpdateProcessOne)
+			processTandS.PUT("all", controllers.UpdateProcessAll)
+			processTandS.PUT("teacher", controllers.UpdateFeedbackTeacher)
 		}
 
 		// ws := main.Group("/ws")
